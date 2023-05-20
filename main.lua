548da6cc18ed173aba8fb1cea616f3e9fdd1e7e3
KeeganAI <53181748+KeeganAI@users.noreply.github.com>
2023-05-20 22:08:05 +0200

    Refactoring
    * Removed give function for each prize in the array, and created a separate function, "givePrizeToPlayer".
    
    * Refactoring of the duplicate code.
    
    * Created the "calculateTotalRawProbability" and "normalizeRawProbabilities" functions, to make the code more readable, modular and editable.
    
    * Added an error check in "normalizeRawProbabilities" to avoid division by zero.

--- main.lua
+++ main.lua
@@ -1,59 +1,60 @@
 -- array of possible prizes. 
--- each prize has an ID, an amount, a message, a raw probability, and a function to execute.
+-- each prize has an ID, an amount, a message and a raw probability
 local Prizes = {
-    {
-        id = "markedbills",
-        amount = 1,
-        message = "general.Marked",
-        rawProbability = 0.05,
-        give = function(xPlayer, src, prize)
-            xPlayer.Functions.AddItem(prize.id, prize.amount)
-            TriggerClientEvent("QBCore:Notify", src, Lang:t("success.You_Won") .. Lang:t(prize.message), "success")
-        end
+    { 
+        id = "markedbills", 
+        amount = 1, 
+        message = "general.Marked", 
+        rawProbability = 0.05 
     },
-    {
-        id = "sandwich",
-        amount = 10,
-        message = "general.Sandwich",
-        rawProbability = 0.2,
-        give = function(xPlayer, src, prize)
-            xPlayer.Functions.AddItem(prize.id, prize.amount)
-            TriggerClientEvent("QBCore:Notify", src, Lang:t("success.You_Won") .. Lang:t(prize.message), "success")
-        end
+    { 
+        id = "sandwich", 
+        amount = 10, 
+        message = "general.Sandwich", 
+        rawProbability = 0.2 
     }
     --... Add more prizes here
 }
 
-
--- Calculate the totalRawProbability by summing the rawProbability of all prizes.
-local totalRawProbability = 0
-for _, prize in ipairs(Prizes) do
-    totalRawProbability = totalRawProbability + prize.rawProbability
+-- function used to give the prize to the player
+-- function to execute in the end
+local function givePrizeToPlayer(player, source, prize)
+    player.Functions.AddItem(prize.id, prize.amount)
+    TriggerClientEvent("QBCore:Notify", source, Lang:t("success.You_Won") .. Lang:t(prize.message), "success")
 end
 
--- Normalize the rawProbability/ies so that they sum to 1.
-for _, prize in ipairs(Prizes) do
-    prize.probability = prize.rawProbability / totalRawProbability
+-- function that calculates the total raw probability
+local function calculateTotalRawProbability(prizes)
+    local total = 0
+    for _, prize in ipairs(prizes) do
+        total = total + prize.rawProbability
+    end
+    return total
 end
 
-local function givePrize(xPlayer, src)
-    -- Generate a random number between 0 and 1.
+-- function that normalizes the raw probabilities
+local function normalizeRawProbabilities(prizes, totalRawProbability)
+    if totalRawProbability == 0 then
+        error("total raw probability is 0, check prizes!")
+    end
+    for _, prize in ipairs(prizes) do
+        prize.probability = prize.rawProbability / totalRawProbability
+    end
+end
+
+local function givePrize(player, source)
     local roll = math.random()
-    
-    -- variable to hold the cumulative probability.
     local cumulativeProbability = 0
 
     for _, prize in ipairs(Prizes) do
-        -- sum the cumulative probability to the normalized probability.
         cumulativeProbability = cumulativeProbability + prize.probability
-        
-        -- Check if the random number is less or equal to the cumulative probability.
-        -- if it is, this prize is selected.
         if roll <= cumulativeProbability then
-            prize.give(xPlayer, src, prize)
+            givePrizeToPlayer(player, source, prize)
             return
-            -- if the generated random number is not less than or equal to the cumulative probability for a given prize, 
-            -- then that prize is not selected, and the code will move on to the next prize in the list.
         end
     end
 end
+
+-- calculate the probabilities
+local totalRawProbability = calculateTotalRawProbability(Prizes)
+normalizeRawProbabilities(Prizes, totalRawProbability)
